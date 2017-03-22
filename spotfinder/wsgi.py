712c88fa114a8edc21f4ef33a6b92497f70f7afb
Pujan Pokhrel <ppokhrel@uno.edu>
2017-03-22 00:18:19 -0500

    added procfile

--- spotfinder/wsgi.py
+++ spotfinder/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spotfinder.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
