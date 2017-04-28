215668fbff111a1e3f4e97aaed71b3abd60c9e81
Pujan Pokhrel <ppokhrel@uno.edu>
2017-04-27 23:13:38 -0500

    hello

--- spots/views.py
+++ spots/views.py
@@ -37,10 +37,11 @@ from django.conf import settings
 def upload(request):
     #global json_received
     if request.method == "POST":
-		data = json.loads(request.body.decode("utf-8"))
-		with open('data.txt', 'w') as outfile:
-			json.dump(data, outfile)
-			return HttpResponse(data)
+		data = request.body.decode("utf-8")
+		request.session['fav_color'] = data
+		#with open('data.txt', 'w') as outfile:
+		#	json.dump(data, outfile)
+		#	return HttpResponse(data)
     else:
     	#data = json.loads(request.body.decode("utf-8"))
     	#with open('data.txt', 'w') as outfile:
@@ -52,16 +53,17 @@ def upload(request):
 
 def get_data(request):
 	#global json_received
-	with open('data.txt') as json_file:  
-		data = json.load(json_file)
-		if data:
-			#print request.session['results']
-			#json_received = request.session.get('results')
-			return JsonResponse(data, safe=False)
-		else:
-			#print json_received
-			#json_received = request.session.get('results')
-			return JsonResponse(data, safe=False)
+	#with open('data.txt') as json_file:  
+	#	data = json.loads(json_file)
+	data = request.session.get('fav_color')
+	if data is not None:
+		#print request.session['results']
+		#json_received = request.session.get('results')
+		return JsonResponse(data, safe=False)
+	else:
+		#print json_received
+		#json_received = request.session.get('results')
+		return JsonResponse(data, safe=False)
 
 
 
