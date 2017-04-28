1c32d0b048e3b957f99d6cd74fde18ff84172975
Pujan Pokhrel <ppokhrel@uno.edu>
2017-04-28 04:06:54 -0500

    values

--- spots/views.py
+++ spots/views.py
@@ -74,6 +74,7 @@ def get_data(request):
 	print data
 	#data = json.dumps(data)
 	print "nothing"
+	data = json.loads(json.loads(data))
 	return JsonResponse(data, safe=False)
 	#	return HttpResponse("No json data Bitch")
 	#return HttpResponse("No json data Bitch")
